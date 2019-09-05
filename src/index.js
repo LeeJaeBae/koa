184128bbc986506fd7458a61518455db7e4fd9c1
LeeJaeBae <nea4182@gmail.com>
2019-09-05 19:37:46 +0900

    first connecting to mongodb

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyPaarser = require('koa-bodyparser');
+const mongoose = require('mongoose');
 
+const { PORT: port = 4000, MONGO_URI: mongoURI } = process.env;
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to mongodb');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 const api = require('./api');
 
 const app = new Koa();
@@ -16,6 +30,6 @@ app.use(bodyPaarser());
 // apply router to app
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
 });

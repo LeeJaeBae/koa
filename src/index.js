9b289347e20529d2c82f559e438bc767c8996f7c
LeeJaeBae <nea4182@gmail.com>
2019-09-05 13:41:20 +0900

    foundation

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const api = require('./api');
+
+const app = new Koa();
+const router = new Router();
+
+// Router setting
+router.use('/api', api.routes());
+
+// apply router to app
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});

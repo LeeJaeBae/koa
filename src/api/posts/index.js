c6d617cc1a1c4a9054f6a9125bc3cd18fbe5914d
LeeJaeBae <nea4182@gmail.com>
2019-09-05 21:16:57 +0900

    connect mongodb

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -4,13 +4,13 @@ const postsCtrl = require('./posts.ctrl');
 const posts = new Router();
 
 // eslint-disable-next-line object-curly-newline
-const { list, write, read, remove, replace, update } = postsCtrl;
+const { list, write, read, remove, checkObjectId, update } = postsCtrl;
 
 posts.get('/', list);
 posts.post('/', write);
-posts.get('/:id', read);
-posts.delete('/:id', remove);
-posts.put('/:id', replace);
-posts.patch('/:id', update);
+posts.get('/:id', checkObjectId, read);
+posts.delete('/:id', checkObjectId, remove);
+// posts.put('/:id', replace);
+posts.patch('/:id', checkObjectId, update);
 
 module.exports = posts;
--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,133 +1,136 @@
-let postId = 1; // initial value of id
+const Joi = require('joi');
+const Post = require('models/post');
+const { ObjectId } = require('mongoose').Types;
 
-const posts = [
-  {
-    id: 1,
-    title: 'title',
-    body: 'Lorem ipsum',
-  },
-];
-
-/* Post
-    Post /api/posts
-    { title,body }
-*/
-
-exports.write = (ctx) => {
-  // Rest ApI의 request body는 ctx.request.body에서 조회할 수 있다.
-  const { title, body } = ctx.request.body;
-  postId += 1;
-
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
-};
-
-/* 포스트 목록 조회
-   GET /api/posts
-*/
-exports.list = (ctx) => {
-  ctx.body = posts;
-};
-
-/* 특정 포스트 조회
-   GET /api/posts/:id
-*/
-exports.read = (ctx) => {
+exports.checkObjectId = (ctx, next) => {
   const { id } = ctx.params;
 
-  // 주어진 id 값으로 포스트를 찾습니다.
-  // 파라미터로 받아 온 값은 문자열 형식이니 파라미터를 숫자로 변환하거나,
-  // 비교할 p.id값을 문자열로 변경해야 합니다.
-  const post = posts.find((p) => p.id.toString() === id);
+  // fail
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; // 400 Bad request
+    return null;
+  }
 
-  // 포스트가 없으면 오류를 반환합니다.
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: "there's no posts",
-    };
+  return next();
+};
+
+// check objectId middleware
+
+// POST /api/posts
+// { title, body, tags }
+
+exports.write = async (ctx) => {
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array()
+      .items(Joi.string())
+      .required(),
+  });
+
+  // first parameter is object to verify, second is schema
+  const result = Joi.validate(ctx.request.body, schema);
+
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
     return;
   }
 
-  ctx.body = post;
+  const { title, body, tags } = ctx.request.body;
+
+  // make a new instance
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (error) {
+    ctx.throw(error, 500);
+  }
 };
 
-/* 특정 포스트 제거
-   DELETE /api/posts/:id
-*/
-exports.remove = (ctx) => {
-  const { id } = ctx.params;
+// GET /api/posts
+exports.list = async (ctx) => {
+  // initial page number is 1
+  // query have to change String to Integer
+  const page = parseInt(ctx.query.page || 1, 10);
 
-  // find id's index number
-  const index = posts.findIndex((p) => p.id.toString() === id);
-
-  // return error message if there's no text
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: "there's no posts",
-    };
+  // if page number is smaller then 1 return error
+  if (page < 1) {
+    ctx.status = 400;
     return;
   }
 
-  // delete index number's item
-  posts.splice(index, 1);
-  ctx.status = 204; // No Content
+  try {
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .exec();
+    const limitBodyLength = (post) => ({
+      ...post.toJSON(),
+      body:
+        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+    });
+    const postCount = await Post.countDocuments().exec();
+    // ctx.set for setting response header
+    ctx.set('Last-Page', Math.ceil(postCount / 10));
+    ctx.body = posts.map(limitBodyLength);
+  } catch (error) {
+    ctx.throw(error, 500);
+  }
 };
 
-/* 포스트 수정
-   PUT /api/posts/:id
-   { title, body }
-*/
-exports.replace = (ctx) => {
-  // using PUT method for change of entire data with inputed post information
+// GET /api/posts/:id
+exports.read = async (ctx) => {
   const { id } = ctx.params;
-
-  // find id's index number
-  const index = posts.findIndex((p) => p.id.toString() === id);
-
-  // return error message if there's no text
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: "there's no posts",
-    };
-    return;
+  try {
+    const post = await Post.findById(id).exec();
+    // if post is not exist
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (error) {
+    ctx.throw(error, 500);
   }
-
-  // 전체 객체를 덮어 씌웁니다.
-  // 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만듭니다.
-  posts[index] = {
-    id,
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
 };
 
-/* 포스트 수정 (특정 필드 변경)
-   PATCH /api/posts/:id
-   { title, body }
-*/
-exports.update = (ctx) => {
+// DELETE /api/posts/:id
+exports.remove = async (ctx) => {
   const { id } = ctx.params;
-
-  // find id's index number
-  const index = posts.findIndex((p) => p.id.toString() === id);
-
-  // return error message if there's no text
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: "there's no posts",
-    };
-    return;
+  try {
+    const post = await Post.findByIdAndRemove(id).exec();
+    // if post is not exist
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (error) {
+    ctx.throw(error, 500);
   }
+};
 
-  // 기존 값에 정보를 덮어씌웁니다.
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
+exports.update = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true,
+    }).exec();
+    // if post is not exist
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (error) {
+    ctx.throw(error, 500);
+  }
 };
